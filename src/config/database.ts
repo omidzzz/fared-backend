8baf75bda575b983f310fc8145a135cf2c697bb1
Sinisteroid <ghadamgahi.omid@gmail.com>
2026-07-02 16:58:10 +0330

    Fix Neon connection - remove PrismaPg adapter

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,10 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const pool = new Pool({
-  connectionString: process.env["DATABASE_URL"],
-});
-
-const adapter = new PrismaPg(pool);
-
 function createPrismaClient(): PrismaClient {
   return new PrismaClient({
-    adapter,
     log:
       process.env["NODE_ENV"] === "development"
         ? ["query", "error", "warn"]
